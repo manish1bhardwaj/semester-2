8af7e20ba27f1377410a21723c68ed150ebf983f
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2024-01-18 20:51:24 +0530

    ..

---
+++ function to tell eligible to vote.py
@@ -0,0 +1,9 @@
+def check_eligible_to_vote(age):
+    return age
+
+#main code
+age = int(input("enter your age"))
+if age>=18:
+    print("you are eligible to vote :")
+else:
+    print("you are not eligible to vote : ")    
